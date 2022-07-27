21fe590fa9ee5e5821c9066bcd3bea13d55f407c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-27 16:47:19 +0530

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -1,39 +1,37 @@
+//Best Case - Array is already sorted O(N)
+//Average Case - O(N*N)
+//Worst Case - Reverse Sorted O(N*N)
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define ld long double
+#define mod 1000000007
 
-void insertion_sort(int arr[], int n){
+void insertionsort(vector<int> &v){
     
-    int flag;
+    int n = v.size();
     
     for(int i = 1; i < n; i++){
-            
-        int curr = arr[i];
-        int j = i-1;
-        
-        while(arr[j] > curr && j >=0){
-            arr[j+1] = arr[j];
+        int j = i - 1;
+        int ele = v[i];
+        while(j >= 0 && v[j] > ele){
+            v[j + 1] = v[j];
             j--;
         }
-        arr[j+1] = curr;
+        j++;
+        v[j] = ele; 
     }
+    
+    for(int i = 0; i < n; i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
 }
 int main() {
-	
-	int n;
-	cin >> n;
-	
-	int arr[n];
-	
-	for(int i = 0; i < n; i++){
-	    cin >> arr[i];
-	}
-	
-	insertion_sort(arr,n);
-	
-	for(auto x : arr){
-	    cout << x << " ";
-	}
-	cout << endl;
-	return 0;
+    
+    vector<int> v = {10,9,8,7,6,5,4,3,2,1};
+   
+    insertionsort(v);
+    return 0;
 }
