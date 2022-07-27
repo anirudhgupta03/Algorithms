9cd9f06838c9018527750e69b2715cf3e40ad207
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-27 16:11:40 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -38,3 +38,42 @@ int main() {
 	cout << endl;
 	return 0;
 }
+
+//Slight Optimisation
+//Best Case - Array is already sorted O(N)
+//Average Case - O(N*N)
+//Worst Case - Reverse Sorted O(N*N)
+//SC - O(N*N)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define mod 1000000007
+
+void  bubblesort(vector<int> &v){
+    
+    int n = v.size();
+    for(int i = 0; i < n; i++){
+        bool flag = false;
+        for(int j = 0; j < n - 1 - i; j++){
+            if(v[j] > v[j + 1]){
+                flag = true;
+                swap(v[j], v[j + 1]);
+            }
+        }
+        if(flag == false) break;
+    }
+    
+    for(int i = 0; i < n; i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    
+    vector<int> v = {10,9,8,7,6,5,4,3,2,1};
+    
+    bubblesort(v);
+    return 0;
+}
