321d3c73a117eec4a30c1a72ae1dfe8d86eee612
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-25 12:32:40 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -77,31 +77,4 @@ int main() {
     bubblesort(v);
     return 0;
 }
-
-//Recursive Bubble Sort
-class Solution {
-  public:
-    void solve(vector<int> &arr, int n){
-        // Base case
-        if (n == 1)
-            return;
-    
-        bool flag = false;
-        for (int i=0; i<n-1; i++){
-            if (arr[i] > arr[i+1]){
-                flag = true;
-                swap(arr[i], arr[i+1]);
-            }
-        }
-        if(flag == false){
-            return;
-        }
-		
-        solve(arr, n-1);
-    }
-    
-    void bubbleSort(vector<int>& arr) {
-        // code here
-        solve(arr, arr.size());
-    }
 };
